75cc2dbbca9145cc3fdeb0fada218fbeb54db188
Aniediabasi Ekanem <eaniedi53@outlook.com>
2022-09-11 11:36:20 -0700

    create 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,25 +4,18 @@
 
 /**
 
-* main - print the string in the put function
+ * main - Entry point
 
-*
+ *
 
-* Description: using the main function
+ * Return: Always 0 (Success)
 
-* this program print "Programming is like building a multilingual puzzle"
-
-* Return: 0
-
-*/
+ */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+    return (0);
 
 }
-
