ff3da3c3329fbd3fefbf5e7f9bb89fbee432d2ff
Aniediabasi Ekanem <eaniedi53@outlook.com>
2022-09-11 11:47:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-    return (0);
-
+	return (0);
 }
