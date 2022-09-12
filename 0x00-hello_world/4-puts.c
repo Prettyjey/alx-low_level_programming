07f67ca392c0a04fbd058e808645400fa36cb4e8
Aniediabasi Ekanem <eaniedi53@outlook.com>
2022-09-12 03:57:42 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,11 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* Return: Always 0 (Success)
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
