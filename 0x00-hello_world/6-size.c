9e1c915a79abf88ee7d19683b5b4f9873d23e241
Aniediabasi Ekanem <eaniedi53@outlook.com>
2022-09-12 16:31:24 -0700

    coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Prints the sizeof many var types
+* main - print the string in the put function
 *
-*Return: Always 0 (Success)
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
-
 int main(void)
 {
 	char a;
